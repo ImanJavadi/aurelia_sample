694b681700348dce558fe450e0f691918b5384d4
ImanJavadi <imanjavadi69@gmail.com>
2021-02-03 14:27:57 +0330

    update validation dialog i18n

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import { Applicant } from './Applicant';
-
+import { PLATFORM } from "aurelia-framework";
 import { inject } from 'aurelia-dependency-injection';
 import { I18N } from "aurelia-i18n";
 import { ValidationControllerFactory, ValidationRules, Validator,validateTrigger  } from 'aurelia-validation';
 import { DialogService } from "aurelia-dialog";
 import { Dialog } from "dialog";
-
+import {RouterConfiguration, Router} from 'aurelia-router';
 interface Applicant1 {
   Name:string;
   Familyname:string;
@@ -27,7 +27,7 @@ export class App {
     Age:'',
     Hired:''
   }
-  
+  router: Router;
   controller: any;
     constructor(private i18n:I18N, controllerFactory: ValidationControllerFactory,private dialogService : DialogService){
       this.controller = controllerFactory.createForCurrentScope();
@@ -84,6 +84,21 @@ export class App {
       } 
    });
   }
+  //router
+  configureRouter(config: RouterConfiguration, router: Router){
+    config.title = 'Contacts';
+    config.options.pushState = true;
+    config.options.root = '/';
+    config.map([
+      { route: '', moduleId: PLATFORM.moduleName('./components/home/home'),   title: 'Select' },
+      { route: 'success',  moduleId: PLATFORM.moduleName('./components/success/success'), name:'success' }
+    ]);
+
+    this.router = router;
+  }
+
+  //
+
  
   action() : void {
     
@@ -111,6 +126,10 @@ export class App {
     this.Applicant.EMailAdress='';
     this.Applicant.Age='';
     this.Applicant.Hired='';
+  }
+  send()
+  {
+    this.router.navigate('success');
   }
  public messege:string='asdsads';
 }
